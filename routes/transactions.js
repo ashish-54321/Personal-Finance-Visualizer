bc92258382880886687fe751945bf45a76528cfd
Ashish Tiwari <115717632+ashish-54321@users.noreply.github.com>
2025-02-19 00:08:56 +0530

    Update transactions.js

--- routes/transactions.js
+++ routes/transactions.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const router = express.Router();
-const Transaction = require('../models/transaction');
+const Transaction = require('../models/Transaction');
 
 // Get all transactions
 router.get('/', async (req, res) => {
